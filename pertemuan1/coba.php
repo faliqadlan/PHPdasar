53b8d29324e5fb4531ce3e71da897db7e7ff9851
faliqadlan <faliq_adlan@student.ub.ac.id>
2020-11-19 12:39:46 +0700

    coba snippet

---
+++ pertemuan1/coba.php
@@ -0,0 +1,25 @@
+<label for="username">username</label>
+<input type="text" name="">
+<?php 
+$this->model->load('m');
+
+$this->load->view('home'); 
+$this->load->model('mahasiswa_model');
+
+?>
+
+<label for="password">Username</label>
+<input type="password" name="password" id="password">
+
+<label for="password">Passord</label>
+<input type="password" name="password" id="password">
+
+<label for="email">Email</label>
+<input type="email" name="email" id="email">
+
+<label>
+    Password
+    <input type="password" name="Password">
+</label>
+
+
