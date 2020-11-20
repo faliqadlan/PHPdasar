3c8d8a8ad549356e4220e5bb1ec330afa772f2df
faliqadlan <faliq_adlan@student.ub.ac.id>
2020-11-20 23:23:34 +0700

    pengulangan 1

---
+++ pertemuan3/latihan1.php
@@ -0,0 +1,24 @@
+<?php 
+// Pengulangan
+// for
+// while
+// do.. while
+// foreach : pengulangan khusus array
+
+// for ($i = 0; $i < 5; $i++) {
+//     echo "$i Hello world! <br>"; //
+// }
+
+// $i = 0;
+// while ($i < 5) {
+//     echo "$i Hello world! <br>";
+// $i++;
+// }
+$a = 0 ;
+do {
+    # code...
+    echo " $a Hello World! <br>";
+    $a+=2;
+} while ($a <= 10);
+
+?>
