514c388bd697635a15dd5090d11426ef4e91cb5f
faliqadlan <faliq_adlan@student.ub.ac.id>
2020-11-21 13:04:42 +0700

    pengondisian

---
+++ pertemuan3/latihan2.php
@@ -0,0 +1,19 @@
+<?php 
+// Pengondisian / Percabangan
+// if else
+// if else if else
+// ternary
+// switch
+
+$x = 20 ;
+if ($x < 20) {
+    echo "benar";
+} elseif ($x == 20) {
+    echo "bingo!";
+} 
+
+else {
+    echo "salah"; # code...
+}
+
+?>
